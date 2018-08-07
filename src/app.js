8f68a23bc270144295b45ca3ab4dbb7fdf9ad0b0
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-08-07 16:49:00 +0100

    creates select_view.js and family_view.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
+const instrumentFamilies = require('./models/instrument_families.js');
+const SelectView = require('./views/select_view.js');
+const FamilyView = require('./views/family_view.js');
+
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript Loaded');
 });
---
+++ src/views/family_view.js
@@ -0,0 +1,7 @@
+const PubSub = require('../helpers/pub_sub.js');
+
+const FamilyView = function(container) {
+  this.container = container;
+};
+
+module.exports = FamilyView;
